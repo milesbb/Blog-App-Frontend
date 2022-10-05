245bec650f47afe0ca013c87c0c29904aaaa44fb
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-10-05 15:49:01 +0100

    added fetch for api

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,12 +1,52 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Alert, Container, Spinner } from "react-bootstrap";
 import BlogList from "../../components/blog/blog-list/BlogList";
 import "./styles.css";
 
 const Home = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [errorOccurred, setErrorOccured] = useState(false);
+  const [authors, setAuthors] = useState([]);
+
+  const getAuthors = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("http://localhost:3004/authors");
+      if (response.ok) {
+        const authors = await response.json();
+        console.log(authors)
+        setAuthors(authors);
+      } else {
+        setErrorOccured(true);
+      }
+    } catch (error) {
+      setErrorOccured(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAuthors();
+  }, []);
+
   return (
     <Container fluid="sm">
       <h1 className="blog-main-title">Welcome to the Strive Blog!</h1>
+
+      {loading && <Spinner animation="border" role="status"></Spinner>}
+      {!loading && errorOccurred && (
+        <Alert variant="danger">Error occurred when fetching authors</Alert>
+      )}
+      {!loading && ! errorOccurred && authors.length === 0 &&
+        <Alert variant="info">No authors found</Alert>
+      }
+      {!loading && !errorOccurred && authors.length > 0 && (
+        <ul>
+          {authors.map(author => <li>{authors.name}</li>)}
+        </ul>
+      )}
+
       <BlogList />
     </Container>
   );
