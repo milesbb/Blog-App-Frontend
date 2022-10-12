11494b5e2fefb4f692e31774a125d56a86a95d1c
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-10-12 15:23:03 +0100

    changed button to a tag

--- src/views/blog/Blog.jsx
+++ src/views/blog/Blog.jsx
@@ -166,7 +166,7 @@ const Blog = ({ authorsArray }) => {
                 __html: blog.content,
               }}
             ></div>
-            <Button onClick={blogPostPdf}>Create Blog Post PDF</Button>
+            <a href={process.env.REACT_APP_BE_URL + "/blogPosts/" + blogId + "/pdf"}>Create Blog Post PDF</a>
             <div>
               <h4>Comments</h4>
               <div style={{ background: "white", borderRadius: "5px" }}>
